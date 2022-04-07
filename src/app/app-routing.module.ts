01e8f87a7ef98ba09e02023caf5beb6a886e66c6
HenrryJK <henrryorbezo@upeu.edu.pe>
2022-04-07 11:09:30 -0500

    #3 Configuracion de Rutas hijas de la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { dashboardRoutes } from "./dashboard/dashboard.routes";
 
 
 /// pasos para crear un sistema de rutas en tu aplicacion si no tienes instalado por defecto un routing modules.
@@ -10,7 +11,11 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
   const routes: Routes =[
     {path: 'login', component: LoginComponent} ,
     {path: 'register',component: RegisterComponent},
-    {path: '', component:DashboardComponent},
+    {
+      path: '',
+      component:DashboardComponent,
+      children:dashboardRoutes
+    },
     {path:'**' , redirectTo:''}
   ];
 
